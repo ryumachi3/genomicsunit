c0debc585f25f9c10fdae41a47e62593bb70ed18
ryumachi3 <good-days_with_big-smile@hotmail.co.jp>
2023-05-11 13:09:04 +0900

    スタッフページコーディング中

--- header.php
+++ header.php
@@ -2,9 +2,13 @@
 <html lang="ja">
 
 <head>
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MDQSLD8');</script>
+  <!-- End Google Tag Manager -->  
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?php bloginfo('name'); ?> | <?php bloginfo('description'); ?></title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.css" />  
   <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/style.css?ver=0.0.6" type="text/css">
   <?php wp_head(); ?>
   <link rel="SHORTCUT ICON" href="https://genomics-unit.pro/wp/wp-content/themes/genomicsunit/img/favicon.ico" />
@@ -20,6 +24,7 @@
   <meta name="twitter:image" content="https://genomics-unit.pro/wp/wp-content/themes/genomicsunit/img/ogp.jpg">
   <script type="text/javascript" src="//webfonts.sakura.ne.jp/js/sakurav3.js"></script>  
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js"></script>  
   <script src="https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll@15.0.0/dist/smooth-scroll.polyfills.min.js"></script>
   <script type="text/javascript">
   // ページ内リンクのみ取得
@@ -37,6 +42,9 @@
 </head>
 
 <body>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MDQSLD8"height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
   <div id="app">
     <!-- <transition name="loadin">
       <div v-if="isloading" class="c-loading p-loading">
---
+++ page-staff.php
@@ -0,0 +1,171 @@
+<?php get_header(); ?>
+<div class="l-main-grid">
+	<?php get_template_part('nav'); ?>
+	<div class="l-main p-main">
+		<div id="contents" class="l-contents">
+      <div class="l-container -staff">
+				<section class="l-common-sec">
+					<h1 class="c-head-title l-head-title l-staff-head-title">
+						<span class="c-head-title__char -navy">ス</span>
+						<span class="c-head-title__char -navy">タ</span>
+						<span class="c-head-title__char -navy">ッ</span>
+						<span class="c-head-title__char -navy">フ</span>
+						<span class="c-head-title__char -navy-frame">紹</span>
+						<span class="c-head-title__char -navy-frame">介</span>
+					</h1>
+					<section class="l-staff-description">
+						<p class="l-staff-description__txt">ゲノム医療ユニットのメンバーを紹介します。</p>
+					</section>
+					<section class="l-doctor-sec p-doctor-sec -sec01">
+						<h2 class="c-title l-doctor-sec__title">医師</h2>
+						<ul class="p-doctor-sec__list">
+							<li class="p-doctor-sec__list__item">
+								<div data-fancybox="gallery"  data-src="#modal_doctor_001" class="p-doctor-sec__list__item__link">
+									<img src="<?php echo get_theme_file_uri() ?>/img/photo_doctor_nishihara.jpg" alt="" class="p-doctor-sec__list__item__img">
+									<div class="p-doctor-sec__list__item__info">
+										<p class="p-doctor-sec__list__item__info__position">教授</p>
+										<h3 class="p-doctor-sec__list__item__info__name">西原 広史</h3>
+										<p class="p-doctor-sec__list__item__info__kana">Hiroshi Nishihara</p>
+										<ul class="p-doctor-sec__list__item__info__field">
+											<li class="p-doctor-sec__list__item__info__field__item">がんゲノム研究</li>
+											<li class="p-doctor-sec__list__item__info__field__item">分子病理学</li>
+										</ul>
+									</div>
+								</div>
+								<div class="modal p-doctor-sec__modal" id="modal_doctor_001">
+									<div class="p-doctor-sec__modal__profile">
+										<img src="<?php echo get_theme_file_uri() ?>/img/photo_doctor_nishihara.jpg" alt="" class="p-doctor-sec__modal__profile__img">
+										<div class="p-doctor-sec__modal__profile__info">
+											<div class="p-doctor-sec__modal__profile__info__name__wrap">
+												<h3 class="p-doctor-sec__modal__profile__info__name">西原 広史</h3>
+												<p class="p-doctor-sec__modal__profile__info__position">教授</p>
+											</div>
+											<p class="p-doctor-sec__modal__profile__info__kana">Hiroshi Nishihara</p>
+											<h4 class="p-doctor-sec__modal__profile__info__field__title">専門/担当分野</h4>
+											<ul class="p-doctor-sec__modal__profile__info__field">
+												<li class="p-doctor-sec__modal__profile__info__field__item">がんゲノム研究</li>
+												<li class="p-doctor-sec__modal__profile__info__field__item">分子病理学</li>
+											</ul>
+										</div>
+									</div>
+									<div class="p-doctor-sec__modal__description">
+										<h4 class="p-doctor-sec__modal__description__title l-doctor-sec__modal__description__title">略歴</h4>
+										<ul class="p-doctor-sec__modal__description__biography p-doctor-sec__modal__description__txt l-doctor-sec__modal__description__txt">
+											<li class="p-doctor-sec__modal__description__biography__item">1995年3月 北海道大学医学部卒業</li>
+											<li class="p-doctor-sec__modal__description__biography__item">1999年3月 北海道大学大学院医学研究科博士課程修了</li>
+										</ul>
+										<h4 class="p-doctor-sec__modal__description__title l-doctor-sec__modal__description__title">略歴</h4>
+										<ul class="p-doctor-sec__modal__description__license p-doctor-sec__modal__description__txt l-doctor-sec__modal__description__txt">
+											<li class="p-doctor-sec__modal__description__license__item">日本病理学会 病理専門医</li>
+											<li class="p-doctor-sec__modal__description__license__item">死体解剖医</li>
+										</ul>
+										<h4 class="p-doctor-sec__modal__description__title l-doctor-sec__modal__description__title">研究業績等</h4>
+										<div class="p-doctor-sec__modal__description__txt l-doctor-sec__modal__description__txt">
+											<p>○○○○○○○○○○○○○○○○○○○○○</p>
+											<p><u>○○○○○○○○○○○○○○○○○○</u></p>
+											<p>○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○</p>
+											<p>○○○○○○○○○○○○○</p>
+											<p><u>○○○○○○○○○○○○○○○○○○○○○</u></p>
+										</div>										
+									</div>
+								</div>
+							</li>
+							<li class="p-doctor-sec__list__item">
+								<div data-fancybox="gallery"  data-src="#modal_doctor_002" class="p-doctor-sec__list__item__link">
+									<div class="p-doctor-sec__list__item__img__wrap">
+										<img src="<?php echo get_theme_file_uri() ?>/img/photo_doctor_hayashi.jpg" alt="" class="p-doctor-sec__list__item__img">
+									</div>
+									<div class="p-doctor-sec__list__item__info">
+										<p class="p-doctor-sec__list__item__info__position">統括マネージャー・特任講師</p>
+										<h3 class="p-doctor-sec__list__item__info__name">林 秀幸</h3>
+										<p class="p-doctor-sec__list__item__info__kana">Hideyuki Hayashi</p>
+										<ul class="p-doctor-sec__list__item__info__field">
+											<li class="p-doctor-sec__list__item__info__field__item">臨床腫瘍学</li>
+											<li class="p-doctor-sec__list__item__info__field__item">ゲノム生物学</li>
+											<li class="p-doctor-sec__list__item__info__field__item">腫瘍内科学</li>
+											<li class="p-doctor-sec__list__item__info__field__item">消化器内科学</li>
+											<li class="p-doctor-sec__list__item__info__field__item">内科学全般</li>
+										</ul>
+									</div>
+									<div class="modal" id="modal_doctor_002">
+										<img src="<?php echo get_theme_file_uri() ?>/img/photo_doctor_hayashi.jpg" alt="" class="p-doctor-sec__list__item__img">
+									</div>
+								</div>
+							</li>
+
+							<!-- <li class="p-doctor-sec__list__item">
+								<a data-fancybox="gallery" data-src="#modal_doctor_001" href="javascript:;" class="p-doctor-sec__list__item__link">
+									<div class="p-doctor-sec__list__item__img__wrap">
+										<img src="<?php echo get_theme_file_uri() ?>/img/photo_doctor_hayashi.jpg" alt="" class="p-doctor-sec__list__item__img">
+									</div>
+									<div class="p-doctor-sec__list__item__info">
+										<p class="p-doctor-sec__list__item__info__position">統括マネージャー・特任講師</p>
+										<h3 class="p-doctor-sec__list__item__info__name">林 秀幸</h3>
+										<p class="p-doctor-sec__list__item__info__kana">Hideyuki Hayashi</p>
+										<ul class="p-doctor-sec__list__item__info__field">
+											<li class="p-doctor-sec__list__item__info__field__item">臨床腫瘍学</li>
+											<li class="p-doctor-sec__list__item__info__field__item">ゲノム生物学</li>
+											<li class="p-doctor-sec__list__item__info__field__item">腫瘍内科学</li>
+											<li class="p-doctor-sec__list__item__info__field__item">消化器内科学</li>
+											<li class="p-doctor-sec__list__item__info__field__item">内科学全般</li>
+										</ul>
+									</div>
+								</a>
+							</li>
+
+							<li class="p-doctor-sec__list__item">
+								<a data-fancybox="gallery" data-src="#modal_doctor_001" href="javascript:;" class="p-doctor-sec__list__item__link">
+									<div class="p-doctor-sec__list__item__img__wrap">
+										<img src="<?php echo get_theme_file_uri() ?>/img/photo_doctor_hayashi.jpg" alt="" class="p-doctor-sec__list__item__img">
+									</div>
+									<div class="p-doctor-sec__list__item__info">
+										<p class="p-doctor-sec__list__item__info__position">統括マネージャー・特任講師</p>
+										<h3 class="p-doctor-sec__list__item__info__name">林 秀幸</h3>
+										<p class="p-doctor-sec__list__item__info__kana">Hideyuki Hayashi</p>
+										<ul class="p-doctor-sec__list__item__info__field">
+											<li class="p-doctor-sec__list__item__info__field__item">臨床腫瘍学</li>
+											<li class="p-doctor-sec__list__item__info__field__item">ゲノム生物学</li>
+											<li class="p-doctor-sec__list__item__info__field__item">腫瘍内科学</li>
+											<li class="p-doctor-sec__list__item__info__field__item">消化器内科学</li>
+											<li class="p-doctor-sec__list__item__info__field__item">内科学全般</li>
+										</ul>
+									</div>
+								</a>
+							</li> -->
+
+
+
+						</ul>
+					</section>
+					<section class="l-staff-sec p-staff-sec -sec02">
+						<h2 class="c-title l-staff-sec__title">スタッフ・共同研究員</h2>
+						<ul class="p-staff-sec__list">
+							<li class="p-staff-sec__list__item">
+									<p class="p-staff-sec__list__item__info__position">共同研究員</p>
+									<h3 class="p-staff-sec__list__item__info__name">○○ ○○</h3>
+									<p class="p-staff-sec__list__item__info__kana">Namae Myouji</p>
+							</li>
+							<li class="p-staff-sec__list__item">
+									<p class="p-staff-sec__list__item__info__position">共同研究員</p>
+									<h3 class="p-staff-sec__list__item__info__name">○○ ○○</h3>
+									<p class="p-staff-sec__list__item__info__kana">Namae Myouji</p>
+							</li>
+							<li class="p-staff-sec__list__item">
+									<p class="p-staff-sec__list__item__info__position">共同研究員</p>
+									<h3 class="p-staff-sec__list__item__info__name">○○ ○○</h3>
+									<p class="p-staff-sec__list__item__info__kana">Namae Myouji</p>
+							</li>
+
+						</ul>
+					</section>
+
+				</section>
+			</div>
+		</div>
+    <!-- contents -->
+  </div>
+  <!-- l-main -->
+</div>
+<!-- l-main-grid -->
+<?php get_footer(); ?>
+<?php get_template_part('wp-footer'); ?>
